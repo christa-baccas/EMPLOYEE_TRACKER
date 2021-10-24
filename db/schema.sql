795e865b25ba0c9cc5c241090f430fe09e54c373
christa-baccas <christa.baccas@gmail.com>
2021-10-24 16:10:58 -0500

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- Creates the department table --
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Creates the role table --
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+-- Creates the employee table --
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role(department_id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+DESCRIBE department;
+DESCRIBE role;
+DESCRIBE employee;
+
